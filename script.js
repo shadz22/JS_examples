d53afcfb7b93766543650290ac1e67226eefab48
shadz22 <shadz22@yahoo.co.uk>
2020-08-23 19:15:07 +0100

    First example

---
+++ script.js
@@ -0,0 +1,11 @@
+let johnScore = 80;
+let nickScore = 45;
+const lowerLimit = 51;
+
+if(johnScore >= lowerLimit) {
+  console.log("John has passed the exam by scoring " + johnScore)
+} else if(nickScore >= lowerLimit) {
+  console.log("Nick has passed the exam by scoring " + nickScore)
+} else {
+  console.log("Both students failed the exam")
+}
