250008a021b47be926c41b3a8a4ca5e9bac78038
shadz22 <shadz22@yahoo.co.uk>
2020-08-25 12:29:53 +0100

    While loop

--- script.js
+++ script.js
@@ -25,3 +25,11 @@ for(var i = 0; i < arr.length; i++) {
   }
   console.log(arr[i]);
 }
+
+//while loop
+let colors = ["blue", "green", "yellow", "pink"]
+var i = 0;
+while(i < colors.length) {
+  console.log(colors[i]);
+  i++
+}
