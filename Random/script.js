66e026fc77b21ee523d9029044105732566d4fd1
shadz22 <shadz22@yahoo.co.uk>
2020-08-26 16:26:48 +0100

    Hoisting examples

--- Random/script.js
+++ Random/script.js
@@ -1,14 +1,14 @@
-let johnScore = 80;
-let nickScore = 45;
-const lowerLimit = 51;
+// let johnScore = 80;
+// let nickScore = 45;
+// const lowerLimit = 51;
 
-if(johnScore >= lowerLimit) {
-  console.log("John has passed the exam by scoring " + johnScore)
-} else if(nickScore >= lowerLimit) {
-  console.log("Nick has passed the exam by scoring " + nickScore)
-} else {
-  console.log("Both students failed the exam")
-}
+// if(johnScore >= lowerLimit) {
+//   console.log("John has passed the exam by scoring " + johnScore)
+// } else if(nickScore >= lowerLimit) {
+//   console.log("Nick has passed the exam by scoring " + nickScore)
+// } else {
+//   console.log("Both students failed the exam")
+// }
 
 // loops
 // let arr = ["John", "Mary", "Jo", "Nick"]
@@ -17,14 +17,14 @@ if(johnScore >= lowerLimit) {
 // }
 
 //break
-let arr = ["John", "Mary", "Bob", "Jo", "Nick"]
-for(var i = 0; i < arr.length; i++) {
-  if(arr[i] === "Bob") {
-    console.log(arr[i] + " is my brother");
-    break
-  }
-  console.log(arr[i]);
-}
+// let arr = ["John", "Mary", "Bob", "Jo", "Nick"]
+// for(var i = 0; i < arr.length; i++) {
+//   if(arr[i] === "Bob") {
+//     console.log(arr[i] + " is my brother");
+//     break
+//   }
+//   console.log(arr[i]);
+// }
 
 //while loop
 // let colors = ["blue", "green", "yellow", "pink"]
@@ -35,9 +35,35 @@ for(var i = 0; i < arr.length; i++) {
 // }
 
 //do while loop
-let colors = ["blue", "green", "yellow", "pink"]
-var i = 0;
-do {
-  console.log(colors[i]);
-  i++;
-} while(i < colors.length);
+// let colors = ["blue", "green", "yellow", "pink"]
+// var i = 0;
+// do {
+//   console.log(colors[i]);
+//   i++;
+// } while(i < colors.length);
+
+
+// Hoisting
+var a = 10;
+ function b() {
+   return 20;
+ }
+ console.log(a);
+ console.log(b());
+ // the above will print 10, then 20 on the console but if we move the console.log to the top the result will change
+
+ console.log(c);
+ console.log(d());
+ var c = 30;
+ function d() {
+   return 40;
+ }
+ // if we run this then we will get undefined, followed by 40
+
+ console.log(e);
+ console.log(f());
+ var e = 50;
+ var f = function() {
+   return 60;
+ }
+ //the above code will return undefined, followed by the error "f is not a function"
