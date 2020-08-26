502e3a641fb2f300f60c83825c8f7d50c0df872e
shadz22 <shadz22@yahoo.co.uk>
2020-08-26 16:54:02 +0100

    The Special Keyword 'this'

--- Random/script.js
+++ Random/script.js
@@ -44,26 +44,74 @@
 
 
 // Hoisting
-var a = 10;
- function b() {
-   return 20;
- }
- console.log(a);
- console.log(b());
- // the above will print 10, then 20 on the console but if we move the console.log to the top the result will change
+// var a = 10;
+//  function b() {
+//    return 20;
+//  }
+//  console.log(a);
+//  console.log(b());
+//  // the above will print 10, then 20 on the console but if we move the console.log to the top the result will change
 
- console.log(c);
- console.log(d());
- var c = 30;
- function d() {
-   return 40;
- }
- // if we run this then we will get undefined, followed by 40
+//  console.log(c);
+//  console.log(d());
+//  var c = 30;
+//  function d() {
+//    return 40;
+//  }
+//  // if we run this then we will get undefined, followed by 40
 
- console.log(e);
- console.log(f());
- var e = 50;
- var f = function() {
-   return 60;
- }
+//  console.log(e);
+//  console.log(f());
+//  var e = 50;
+//  var f = function() {
+//    return 60;
+//  }
  //the above code will return undefined, followed by the error "f is not a function"
+
+
+ //this
+
+function a() {
+  console.log(this);
+  console.log(this === window);
+
+  function b() {
+    console.log(this);
+    console.log(this === window);
+  }
+
+  b();
+
+}
+
+a();
+
+
+var firstname = 'Bob';
+
+var per = {
+  firstname: 'John',
+  lastname: 'Smith',
+  getFullName: function() {
+    var that = this;
+    // console.log(this === per);
+    // return per.firstname + ' ' + per.lastname;
+    // return this.firstname + ' ' + this.lastname;
+    console.log(this.firstname + ' ' + this.lastname);
+
+    function greet() {
+      console.log('Hi ' + that.firstname);
+    }
+    greet();
+  }
+};
+
+var per1 = {
+  firstname: 'Nick',
+  lastname: 'Doe'
+};
+
+per1.getFullName = per.getFullName;
+
+per.getFullName();
+per1.getFullName();
