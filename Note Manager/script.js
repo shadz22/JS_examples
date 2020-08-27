08e8a086bb1c79a4401698f0fac12b03a2158c8b
shadz22 <shadz22@yahoo.co.uk>
2020-08-27 17:05:25 +0100

    Examples of 'querySelector'

--- Note Manager/script.js
+++ Note Manager/script.js
@@ -1,18 +1,32 @@
 // .textContent
-console.log(document.getElementById('list'));
+// console.log(document.getElementById('list'));
 
-var el = document.getElementById('list');
-console.log(el);
-console.log(typeof el);
-
-// var el = document.getElementById('LIST');
+// var el = document.getElementById('list');
 // console.log(el);
+// console.log(typeof el);
 
-console.log(el.textContent);
-console.log(el.textContent = 'Hello');
+// console.log(el.textContent);
+// console.log(el.textContent = 'Hello');
 
-var heading = document.getElementById('heading').textContent;
-console.log(heading);
+// var heading = document.getElementById('heading').textContent;
+// console.log(heading);
 
-var ulElement = document.getElementById('list');
-console.log(ulElement.textContent = heading);
+// var ulElement = document.getElementById('list');
+// console.log(ulElement.textContent = heading);
+
+
+
+var el = document.querySelector('ul');
+console.log(el);
+console.log(el.textContent = 'Hello World');
+
+var el = document.querySelector('#search-note');
+console.log(el);
+
+var el = document.querySelector('ul li');
+console.log(el);
+
+var el = document.querySelector('ul li:nth-child(2)');
+console.log(el);
+
+console.log(el.querySelector('p .fa-times'));
