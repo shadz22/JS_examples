fe4404bda52e07c7e16b79c913adfb271bea7556
shadz22 <shadz22@yahoo.co.uk>
2020-09-09 18:34:05 +0100

    Examples of Function Constructors and Prototypes

--- Random/script.js
+++ Random/script.js
@@ -181,14 +181,52 @@
 // console.log(typeof strObj);
 
 
-//Prototype - Object Function
-var a = {};
-var b = new Object();
+// //Prototype - Object Function
+// var a = {};
+// var b = new Object();
 
-console.log(a);
-console.log(b);
-console.log(Object);
-console.log(Object.prototype);
+// console.log(a);
+// console.log(b);
+// console.log(Object);
+// console.log(Object.prototype);
 
-Object.prototype.greet = 'Hello';
-console.log(Object.prototype);
+// Object.prototype.greet = 'Hello';
+// console.log(Object.prototype);
+
+
+// Function Constructors And Prototypes
+function a() {}
+
+console.log(a.prototype);
+console.log(a.prototype.__proto__);
+
+var b = {};
+
+function Person(firstname, lastname) {
+  this.firstname = firstname;
+  this.lastname = lastname;
+  // this.getFullName = function() {
+  //   return this.firstname + ' ' + this.lastname;
+  // }      //the better way of doing this would be to use prototype as per below:
+}
+
+Person.prototype.getFullName = function() {
+  return this.firstname + ' ' + this.lastname;
+}
+
+Person.prototype.greet = 'Hello from prototype object';
+
+var person1 = new Person('John', 'Smith');
+person1.greet = 'Hello from person1 object';
+console.log(person1.greet);
+var person2 = new Person('Nick', 'Doe');
+
+console.log(person1.getFullName());
+console.log(person2.getFullName());
+console.log(person1.greet);
+
+
+
+
+
+
