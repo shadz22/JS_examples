e1f19bb9b33a65a1b29d9696b397499bab2b946d
shadz22 <shadz22@yahoo.co.uk>
2020-09-11 19:25:02 +0100

    Examples of different types of functions

--- Random/script.js
+++ Random/script.js
@@ -229,6 +229,7 @@ console.log(person1.greet);
 */
 
 // Object.create()
+/*
 function Person() {
   this.name = "John";
 }
@@ -272,6 +273,63 @@ console.log(bob);
 
 var obj = Object.create(null);
 console.log(obj);
+*/
+
+
+// First-Class Functions
+var a = function() {
+  console.log('Hello');
+}
+
+a();
+
+function sum(num1, num2, fn) {
+  console.log(num1 + num2);
+  console.log(fn());
+}
+
+function done() {
+  return 'Function is executed'
+}
+
+sum(5, 10, done);
+sum(5, 10, function() {
+  return 'Function is executed'
+});
+
+var scores = [55, 35, 87, 45];
+
+function checkResult(arr, fn) {
+  var newArr = [];
+  for(var i = 0; i < arr.length; i++) {
+    newArr.push(fn(arr[i]));
+  }
+  return newArr;
+}
+
+function passOrFail(score) {
+  return score >= 51;
+}
+
+function calcDifference(score) {
+  return score - 51;
+}
+
+console.log(checkResult(scores, passOrFail));
+console.log(checkResult(scores, calcDifference));
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
